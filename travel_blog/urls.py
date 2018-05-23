d361e8358d4d6783df0ed2641f62ef0363ca4974
zmomen <zmomen@andrew.cmu.edu>
2018-05-23 11:10:00 -0400

    Add files via upload

---
+++ travel_blog/urls.py
@@ -0,0 +1,33 @@
+"""travel_blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf.urls import url, include
+from travel_blog import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^$', views.login_redirect, name='login_redirect'),
+    url(r'^account/', include('accounts.urls', namespace='accounts'), name='account'),
+    url(r'^home/', include('home.urls', namespace='home')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^vote/', include('vote.urls', namespace='vote')),
+    url(r'^summernote/', include('django_summernote.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
